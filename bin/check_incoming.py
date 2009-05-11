f17736e38d36f2d080f9bb07f760e747953d9204
Joao Pinto <joao.pinto@getdeb.net>
2009-05-11 19:02:35 +0100

    Added the new incoming queue check script

--- bin/check_incoming.py
+++ bin/check_incoming.py
@@ -15,95 +15,139 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    --------------------------------------------------------------------
-"""  
+"""
   This script will check the ftp incoming directoy for debian source
   packages. 
+  When *_source.changes is found, it's contents are verified and
+  the files are moved to the pre_build queue.
+  
   The expected structure is ftp_incoming_dir/release_name, 
     eg: /home/ftp/incoming/jaunty
+  
   Files will be verified with the following rules
-  Acquire exclusive lock incoming_lock_file, exit on failure
 	For each release directory		
 		Get list of files into filelist
-		For every *_source.changes
-			Create a sub list of files described on the _source.changes
-			Append the _soure.changes to the sublist
-			Verify GPG signature using ~/debfactory/keyrings/uploaders.gpg
-			If GPG is ok, action=move else action=delete, log failure
-			For each file on the sublist:
-				Verify MD5
-				If MD5 fails, action=delete, log md5 failure
-			For each file on the sublist:
-				Apply and log action 
-			If GPG is ok, send email to GPG signer with the action log			
-		For every file on filelist
-			If file is older than 10s, try do delete it			
-   Close and release lock /tmp/check_incoming.lock
+		...
+		
+   We use a file lock to aboid paralell runs		
+  
 """
-import os, sys, commands
+import os, sys, commands, shutil
 import commands
 from optparse import OptionParser
+from configobj import ConfigObj
 from localaux import *
 import glob
 import dpkg
+import time
 
-ftp_incoming_dir = '/home/ftp/incoming'
-pre_build_dir = "/build/pre_build"
-incoming_lock_file = '/tmp/check_incoming_lock_file.lock'
+config_file = "%s/debfactory/etc/debfactory.conf" % os.environ['HOME']
+config = ConfigObj(config_file)
+
+# Load configuration
+try:
+	upload_status_email = config['upload_status_email']
+	ftp_incoming_dir = config['ftp_incoming_dir']
+	pre_build_dir = config['pre_build_dir']
+except:
+	print "Configuration error"
+	print `sys.exc_info()[1]`
+	sys.exit(3)
+	
+incoming_lock_file = '/tmp/check_incoming_lock_file.pid'
+
+# Clean up all files older than 24h
+CLEANUP_TIME = 24*3600
 
 gpg_ops = '--no-options --no-default-keyring --keyring '+os.environ['HOME']+'/debfactory/keyrings/uploaders.gpg '
 os.putenv('LANG', 'C') # We check system command replies, use a reliable language
 
 Log = Logger()	
 
-def check_source_changes(changes_file):
+def check_source_changes(changes_file, release):
 	"""	
 	Check a _source.changes file and proceed as described in the script
 	action flow . 
 	"""
+	target_mails = [upload_status_email]
 	Log.log("Checking "+changes_file)	
-	(rc, output) = commands.getstatusoutput('gpg '+gpg_ops+' --verify --logger-fd=1 '+changes_file)
+	report_msg = "File: %s\n" % changes_file
+	report_msg  += '-----------------\n'
+	(rc, output) = commands.getstatusoutput('gpg '+gpg_ops+' --verify --logger-fd=1 '+changes_file)	
 	output_lines = output.split("\n")
 	sign_author = None
 	for line in output_lines:		
 		if line.startswith("gpg: Good signature from"):
 			print line
 			dummy, sign_author, dummy = line.split('"')	
-	if rc<>0 or sign_author==None:
-		print "ERROR: Unable to verify GPG key for ",changes_file		
+	if rc<>0 or not sign_author:
+		print output
+		print "ERROR: Unable to verify GPG key for ", changes_file				
 		return
+	target_mails.append(sign_author)
+	report_msg  += "Signed By: %s\n" % sign_author
+
+		
+	# Get list of files described on the changes
+	report_title = "%s upload FAILED\n" % os.path.basename(changes_file)
+	report_msg += "List of files:\n"	
+	report_msg += "--------------\n"
 	file_list = dpkg.get_files_list(changes_file)		
 	base_dir = os.path.dirname(changes_file)
-	for file in file_list:
-		filename = base_dir+"/"+file[4]
-		if not os.path.exists(filename):
-			print 'ERROR: Could not find '+filename+" listed at ", changes_file
-			#send_error("err_file", package, sign_author)
-			return
-		md5sum = check_md5sum(filename, file[0])
-		if md5sum:
-			print 'ERROR: File '+filename,+' has md5sum '+md5sm+' expected '+file[0]
-			return			
-	orig_file = dpkg.getOrigTarGzName(changes_file)
 	
-	if not orig_file:
+	filename_list = []	
+	
+	# Check if orig_file is available
+	orig_file = dpkg.getOrigTarGzName(changes_file)	
+	if not orig_file:		
 		return
-	if os.path.exists(base_dir+"/"+orig_file):
-		file_list.append(orig_file)
+					
+	if os.path.exists("%s/%s" % (base_dir, orig_file)):
+		filename_list.append("%s/%s" % (base_dir,orig_file))
 	else:
-		if not os.path.exists(pre_build_dir+"/"+orig_file):
-			print 'ERROR: Missing  '+orig_file+' for '+changes_file		
+		if not os.path.exists("%s/%s/%s" % (pre_build_dir, release, orig_file)):
+			print "ERROR: Missing %s for %s" % (orig_file, changes_file)
+			report_msg += "ERROR: Missing %s for %s\n" % (orig_file, changes_file)
+			send_mail_message(target_mails, report_title, report_msg)
 			return
 		else:
 			Log.log('No orig.tar.gz, using the one already available '+ base_dir+"/"+orig_file)
-			
-	# Source package passed all tests, let's move it to pre_build	
-	for file in uniq(ile_list):
-		print 'Moving ',base_dir+"/"+file,'->',pre_build_dir
+	
+	for file in file_list:
+		report_msg += "%s - " % file[4]		
+		filename = base_dir+"/"+file[4]
+		filename_list.append(filename)		
+		if not os.path.exists(filename):
+			print 'ERROR: Could not find '+filename+" listed at ", changes_file			
+			#send_error("err_file", package, sign_author)
+			report_msg += "Missing\n"			
+			send_mail_message(target_mails, report_title, report_msg)
+			return
+		md5sum = check_md5sum(filename, file[0])
+		if md5sum:
+			print md5sum
+			print 'ERROR: File %s md5sum is %s, expected %s' % (filename, md5sum, file[0])
+			report_msg += "MD5 mismatch\n"
+			send_mail_message(target_mails, report_title, report_msg)
+			return			
+		report_msg += "OK\n"		
+
+	# Source package passed all tests, let's move it to pre_build		
+	target_dir = "%s/%s" % (pre_build_dir, release)
+	if not os.path.exists(target_dir):
+		os.mkdir(target_dir)
+	for file in uniq(filename_list):
+		print 'Moving ', file,'-> '+target_dir
+		if os.path.exists("%s/%s" % (target_dir, os.path.basename(file))):
+			os.unlink("%s/%s" % (target_dir, os.path.basename(file)))
 		try:
-			shutil.move(base_dir+"/"+file, pre_build_dir)
+			shutil.move(file, target_dir)
 		except:
 			print "FAILED: "+`sys.exc_info()[1]`
 			return
+			
+	report_title = "%s upload SUCCESSFUL\n" % os.path.basename(changes_file)
+	send_mail_message(target_mails, report_title, report_msg)	
 				
 def check_release_dir(releasedir):
 	""" 
@@ -112,10 +156,20 @@ def check_release_dir(releasedir):
 	"""
 	Log.log("Checking "+releasedir)
 	file_list = glob.glob(releasedir+"/*")
+	release = os.path.basename(releasedir)
 	change_list = []
 	for fname in file_list:
 		if fname.endswith("_source.changes"):
-			check_source_changes(fname)	
+			check_source_changes(fname, release)	
+			os.unlink(fname)
+		else:
+			if time.time() - os.path.getmtime(fname) > CLEANUP_TIME:
+				print "Removing old file: %s", fname
+				try:
+					os.unlink(fname)
+				except:
+					print "FAILED: "+`sys.exc_info()[1]`			
+	Log.log("Done")
 	
 def main():
 	parser = OptionParser()
@@ -130,9 +184,7 @@ def main():
 	# Get list of releases
 	releases = glob.glob(ftp_incoming_dir+'/[a-z]*')
 	for release in releases:
-		check_release_dir(release)
-	# Release the lock file
-	lock.Close()
+		check_release_dir(release)		
 			
 if __name__ == '__main__':
 	try:
--- bin/localaux.py
+++ bin/localaux.py
@@ -16,10 +16,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    --------------------------------------------------------------------
 """
-Local auxiliary functions library
+Local auxiliar functions library
 """
 import os
 import sys
+import commands
+import smtplib
+import atexit
 
 def uniq(alist):
     set = {}
@@ -33,6 +36,7 @@ class Logger:
 		if self.verbose:
 			print message	
 
+		
 """ Lock file management class"""
 class LockFile:
 	def __init__(self, Log, lock_filename):
@@ -41,24 +45,49 @@ class LockFile:
 		if os.path.islink(lock_filename):
 			print 'FATAL ERROR: symlink '+lock_filename
 			sys.exit(2)
-		Log.log('Acquiring lock on file ' + lock_filename)	
-
-		oflags = os.O_WRONLY|os.O_NONBLOCK|os.O_CREAT
+		#oflags = os.O_EXCL | os.O_RDWR |os.O_NONBLOCK| os.O_CREAT
+		oflags = os.O_RDWR |os.O_NONBLOCK| os.O_CREAT | os.O_EXCL
 		try:	
-			self.lock_fd = os.open(lock_filename, oflags, 0007)
+			self.lock_fd = os.open(lock_filename, oflags)
+			os.write(self.lock_fd, "%d\n" % os.getpid())
 		except:
+			Log.log("Unable to acquire lock %s" % lock_filename)
 			Log.log("FAILED: "+`sys.exc_info()[1]`)
 			sys.exit(2)
-	def Close(self):
-		os.close(self.lock_fd)
-		os.unlink(self.lock_filename)
+		atexit.register(self.lock_remove, lock_filename, self.lock_fd)
+	def lock_remove(self, fname, lock_fd):
+		try:
+			os.close(lock_fd)
+			os.unlink(fname)
+		except:
+			pass
+
 		
 def check_md5sum(filename, expected_md5sum):
 	"""
 	"""
-	md5sum=commands.getoutput('md5sum '+filename,"r")
-	(newmd5, dummy) = string.split(md5sum)
+	md5sum=commands.getoutput('md5sum '+filename)
+	(newmd5, dummy) = md5sum.split()
 	#newmd5 = newmd5.strip('\r\n')
 	if newmd5 != expected_md5sum:     	
 		return newmd5
 	return None
+
+def send_email(toaddrs, message):
+    fromaddr = '"GetDeb Automated Builder" <autobuild@getdeb.net>'
+    server = smtplib.SMTP('localhost')
+    server.sendmail(fromaddr, toaddrs, message)
+    server.quit()
+	
+def send_mail_message(destination, subject, body):
+	"""
+	Sends a mail message
+	"""
+	
+	message = "Subject: %s\n\n" % subject
+	message += body
+	if type(destination) is list:
+		for dest in destination:
+			send_email(dest, message)		
+	else:
+		send_email(destination, message)		
