b94553747bad2b47ff4778764c803a02c43655ba
Joao Pinto <joao.pinto@getdeb.net>
2009-10-24 21:37:09 +0100

    Return sign_author as unicode

--- bin/dpkg_control.py
+++ bin/dpkg_control.py
@@ -151,7 +151,7 @@ class DebianControlFile:
 			for line in output_lines:		
 				if line.startswith("gpg: Good signature from"):
 					dummy, sign_author, dummy = line.split('"')	
-		return sign_author
+		return unicode(sign_author, 'utf-8', errors='replace')
 		
 	def verify_md5sum(self, source_dir=None):
 		"""
