cc5d18d46fccc00dfe49b0a9f3acefb7a6d93143
Christoph Korn <c_korn@gmx.de>
2014-03-01 15:35:25 +0100

    get_diff_gz.py: Create top level dir or rename
    Sometimes the top level directory in a tarball has an unexpected name
    or does not exist at all. Make sure everything is set up properly.

--- bin/get_diff_gz.py
+++ bin/get_diff_gz.py
@@ -33,11 +33,11 @@ def get_base_package_name():
 	if not tmp: return None
 	return os.path.basename(tmp[0])
 
-def search_on_getdeb(orig_file):
+def search_on_mirror(orig_file, section):
 	global MIRROR_URL, GETDEB_SUBDIR
 	http_connection = HTTPConnection(MIRROR_URL)
 	basename = orig_file.split('_')[0]
-	download_dir = 'http://' + MIRROR_URL + '/' + GETDEB_SUBDIR + '/ubuntu/pool/apps/' + get_package_subdir(orig_file) + \
+	download_dir = 'http://' + MIRROR_URL + '/' + GETDEB_SUBDIR + '/ubuntu/pool/' + section + '/' + get_package_subdir(orig_file) + \
 	  '/' + basename + '/'
 	http_connection.request('GET', download_dir)
 	http_response = http_connection.getresponse()
@@ -62,58 +62,62 @@ def search_on_getdeb(orig_file):
 		p_d.append((orig,download))
 	return p_d
 
-def search_on_playdeb(orig_file):
-	global MIRROR_URL, GETDEB_SUBDIR
-	http_connection = HTTPConnection(MIRROR_URL)
-	basename = orig_file.split('_')[0]
-	download_dir = 'http://' + MIRROR_URL + '/' + GETDEB_SUBDIR + '/ubuntu/pool/games/' + get_package_subdir(orig_file) + \
-	  '/' + basename + '/'
-	http_connection.request('GET', download_dir)
-	http_response = http_connection.getresponse()
-	if http_response.status != 200: return None
-	data = http_response.read()
-	http_connection.close()
-	data = data.split('\n')
-	package_lines = list()
-	for line in data:
-		if basename in line:
-			package_lines.append(line)
-	if len(package_lines) == 0: return None
-	p_d = list()
-	package_re = re.compile('<a .*?>(?P<orig>.*?)(?:\.diff\.gz|\.debian\.tar\.gz)<')
-	download_re = re.compile('<a href="(?P<download>.*?)">')
-	for line in package_lines:
-		search_result = re.search(package_re, line)
-		if not search_result: continue
-		orig = search_result.group('orig') 
-		search_result = re.search(download_re, line)
-		download = download_dir + search_result.group('download')
-		p_d.append((orig,download))
-	return p_d
+def untar(t, orig):
+	s = 'tar x%(t)sf %(orig)s'%locals()
+	print s
+	os.system(s)
+
+def topLevelName(files):
+	name = None
+	for f in files:
+		if f:
+			cur = f.split("/", 1)
+			# no top level dir. Just files?
+			if cur[0] == f: return None
+			if cur[0] != name:
+				# init
+				if name is None:
+					name = cur[0]
+				# there is a different top level name
+				# so the tarball does not have a top level dir
+				else:
+					return None
+	return name
 
 def applyDiff(p,orig):
 	package_name = orig.split('_')[0]
 	version = orig.split('_')[1].split('.orig')[0]
 	dir_name = package_name + '-' + version
+	tarType = None
+	if orig.endswith(".gz"): tarType = "z"
+	elif orig.endswith(".bz2"): tarType = "j"
+	elif orig.endswith(".xz"): tarType = "J"
+	else: return
+	cwd = os.getcwd()
 	if not os.path.exists(dir_name):
-		if orig.endswith(".gz"):
-			print 'tar xzf ' + orig
-			os.system('tar xzf ' + orig)
-		elif orig.endswith(".bz2"):
-			print 'tar xjf ' + orig
-			os.system('tar xjf ' + orig)
-		elif orig.endswith(".xz"):
-			print 'tar xJf ' + orig
-			os.system('tar xJf ' + orig)
-		if not os.path.exists(dir_name):
-			tmp = glob.glob('./' + package_name + '*')
-			for t in tmp:
-				if os.path.isdir(t):
-					os.rename(t, dir_name)
+		# has the top-level directory the correct name
+		# or is there any at all?
+		p1 = Popen(["tar", "tf", orig], stdout=PIPE)
+		top_level_dir = topLevelName(p1.communicate()[0].split("\n"))
+		# no top level dir? create it, cd in it, untar, go back
+		if top_level_dir is None:
+			print "No top level dir. Creating: %(dir_name)s"%locals()
+			os.mkdir(dir_name)
+			os.chdir(os.path.join(cwd, dir_name))
+			untar(tarType, os.path.join("..", orig))
+			os.chdir(cwd)
+		# top level dir has wrong name? untar and rename
+		elif top_level_dir != dir_name:
+			untar(tarType, orig)
+			print "Top level dir has wrong name. Renaming: %(top_level_dir)s -> %(dir_name)s"%locals()
+			os.rename(top_level_dir, dir_name)
+		# all fine. top level dir exists and has correct name
+		else:
+			untar(tarType, orig)
 	if not os.path.exists(dir_name + '/debian'):
 		tmp = glob.glob(p + '*.diff.gz')
 
-		cwd = os.getcwd()
+
 		os.chdir(os.path.join(cwd, dir_name))
 		if tmp:
 			print 'zcat ../' + p + '*.diff.gz | patch -p1'
@@ -131,6 +135,10 @@ def applyDiff(p,orig):
 
 		print 'dch -D ' + release + ' --newversion "'+version+'-1~getdeb1" "New upstream version"'
 		os.system('dch -D ' + release + ' --newversion "'+version+'-1~getdeb1" "New upstream version"')
+	else:
+		print "Warning! debian directory already exists. Not replacing."
+
+	os.chdir(cwd)
 
 def diffVersionCompare(a,b):
 	v1 = a[0].split("_")[1]
@@ -147,8 +155,8 @@ if __name__ == "__main__":
 		print "No orig.tar.gz file has been found."
 		sys.exit(1)
 
-	result = search_on_getdeb(orig_file) or []
-	result2 = search_on_playdeb(orig_file) or []
+	result = search_on_mirror(orig_file, "apps") or []
+	result2 = search_on_mirror(orig_file, "games") or []
 	result.extend(result2)
 
 	result.sort(key=cmp_to_key(diffVersionCompare))
